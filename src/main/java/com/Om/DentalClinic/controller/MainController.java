ce8c087e69894f9997044f76c3b61269e868ae26
Mansi <mansi.desai@techentrepreneurs.co.in>
2023-09-01 11:44:33 +0530

    2nd API for saving data is working postAPI patientinfo with file is getting saved into database

--- src/main/java/com/Om/DentalClinic/controller/MainController.java
+++ src/main/java/com/Om/DentalClinic/controller/MainController.java
@@ -1,13 +1,19 @@
 package com.Om.DentalClinic.controller;
 
+import java.io.IOException;
+import java.util.Date;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.format.annotation.DateTimeFormat;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
-
+import org.springframework.web.multipart.MultipartFile;
 import com.Om.DentalClinic.model.PatientInfo;
 import com.Om.DentalClinic.service.PatientInfoService;
+
 
 @RestController
 public class MainController {
@@ -20,7 +26,19 @@ public class MainController {
 	public List<PatientInfo> getAllPatientInfo() {		
 		return  this.patientInfoService.getAllPatientInfo();
 	}	
-	
-	
+
+	@PostMapping("/SavePatientInfo")
+	public String savePatientInfo(@RequestParam("file") MultipartFile file,
+	@RequestParam("patientnumber") String patientnumber,
+	@RequestParam("patientname") String patientname,
+	@RequestParam("patientage") int patientage,
+	@RequestParam("patientgender") String patientgender,
+	@RequestParam("patientregdate") @DateTimeFormat(pattern = "yyyy-MM-dd'T'HH:mm:ss.SSSXXX") Date patientregdate,
+	@RequestParam("patientmobile") Long patientmobile,
+	@RequestParam("patientmedicalhistory") String patientmedicalhistory) throws IOException
+	{
+		return patientInfoService.savePatientInfo(file,patientnumber,patientname,patientage,patientgender,patientregdate,patientmobile,patientmedicalhistory);
+	}
+
 	
 }
--- src/main/java/com/Om/DentalClinic/service/PatientInfoService.java
+++ src/main/java/com/Om/DentalClinic/service/PatientInfoService.java
@@ -1,14 +1,20 @@
 package com.Om.DentalClinic.service;
 
+import java.io.IOException;
+import java.util.Date;
 import java.util.List;
+
+import org.springframework.web.multipart.MultipartFile;
 
 import com.Om.DentalClinic.model.PatientInfo;
 
 public interface PatientInfoService {
 
 	public List<PatientInfo> getAllPatientInfo();
-	public void savePatientInfo(PatientInfo patientInfo) ;
-	public PatientInfo getPatientInfoById(Long id) ;
-	public void deletePatientInfoById(Long id) ;
+
+	public String savePatientInfo(MultipartFile file,String patientnumber,String patientname,int patientage, String patientgender,Date patientregdate,Long patientmobile,String patientmedicalhistory) throws IOException;
+//	public PatientInfo getPatientInfoById(Long id) ;
+//	public void deletePatientInfoById(Long id) ;
 
 }
+
