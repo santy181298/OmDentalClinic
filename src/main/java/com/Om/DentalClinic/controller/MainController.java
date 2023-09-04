7ffc71b061e3d9ec9deb1776188de9d4e4264a7d
Vaibhavi22-c <vzende89@gmail.com>
2023-09-04 16:11:41 +0530

    PatientDetails and ProcedureDetails UI forms by Vaibhavi

--- src/main/java/com/Om/DentalClinic/controller/MainController.java
+++ src/main/java/com/Om/DentalClinic/controller/MainController.java
@@ -6,6 +6,7 @@ import java.util.List;
 import java.util.Optional;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.format.annotation.DateTimeFormat;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -23,47 +24,54 @@ import com.Om.DentalClinic.service.PatientInfoService;
 import com.Om.DentalClinic.service.PatientProcedureService;
 
 
-@RestController
+@Controller
 public class MainController {
 
-	@Autowired
-	private PatientInfoService patientInfoService;
+//	@Autowired
+//	private PatientInfoService patientInfoService;
+//	
+//	@Autowired
+//	private PatientProcedureService patientProcedureService;
+//	
+//	@GetMapping("/listPatientInfo")
+//	public List<PatientInfo> getAllPatientInfo() {		
+//		return  this.patientInfoService.getAllPatientInfo();
+//	}	
 	
-	@Autowired
-	private PatientProcedureService patientProcedureService;
 	
-	@GetMapping("/listPatientInfo")
-	public List<PatientInfo> getAllPatientInfo() {		
-		return  this.patientInfoService.getAllPatientInfo();
-	}	
-	
-	
-	// Patient Procedure Controller
-		@GetMapping("/home")
-		public String procedureHome() {
-			return "This is Patient Procedure home page";
-			
-		}
-		
-	 @GetMapping("/")
-	    public List<PatientProcedure> getAllProcedures() {
-	        return patientProcedureService.getAllProcedures();
+//	// Patient Procedure Controller
+//		@GetMapping("/home")
+//		public String procedureHome() {
+//			return "This is Patient Procedure home page";
+//			
+//		}
+//		
+//	 @GetMapping("/")
+//	    public List<PatientProcedure> getAllProcedures() {
+//	        return patientProcedureService.getAllProcedures();
+//	 }
+	 
+	 @GetMapping("/procedureDetails")
+	 public String showProcedureDetail() {
+		 return "procedureDetails";
+	 }
+	 @GetMapping("/patientDetails")
+	 public String showPatientDetail() {
+		 return "patientDetails";
 	 }
 
+//	@PostMapping("/SavePatientInfo")
+//	public String savePatientInfo(@RequestParam("file") MultipartFile file,
+//	@RequestParam("patientnumber") String patientnumber,
+//	@RequestParam("patientname") String patientname,
+//	@RequestParam("patientage") int patientage,
+//	@RequestParam("patientgender") String patientgender,
+//	@RequestParam("patientregdate") @DateTimeFormat(pattern = "yyyy-MM-dd'T'HH:mm:ss.SSSXXX") Date patientregdate,
+//	@RequestParam("patientmobile") Long patientmobile,
+//	@RequestParam("patientmedicalhistory") String patientmedicalhistory) throws IOException
+//	{
+//		return patientInfoService.savePatientInfo(file,patientnumber,patientname,patientage,patientgender,patientregdate,patientmobile,patientmedicalhistory);
+//	}
 
-	@PostMapping("/SavePatientInfo")
-	public String savePatientInfo(@RequestParam("file") MultipartFile file,
-	@RequestParam("patientnumber") String patientnumber,
-	@RequestParam("patientname") String patientname,
-	@RequestParam("patientage") int patientage,
-	@RequestParam("patientgender") String patientgender,
-	@RequestParam("patientregdate") @DateTimeFormat(pattern = "yyyy-MM-dd'T'HH:mm:ss.SSSXXX") Date patientregdate,
-	@RequestParam("patientmobile") Long patientmobile,
-	@RequestParam("patientmedicalhistory") String patientmedicalhistory) throws IOException
-	{
-		return patientInfoService.savePatientInfo(file,patientnumber,patientname,patientage,patientgender,patientregdate,patientmobile,patientmedicalhistory);
-	}
-
-	
->>>>>>> branch 'master' of https://github.com/santy181298/OmDentalClinic.git
+	//github.com/santy181298/OmDentalClinic.git
 }
